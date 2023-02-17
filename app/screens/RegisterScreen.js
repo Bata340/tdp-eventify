83acd94b344978749984f0dac23db55b35f1d46b
manulon <mlongoe@fi.uba.ar>
2023-02-17 16:33:23 -0300

    fecha de nacimiento en el registro

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, ScrollView, Image, View, Alert } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, ScrollView, Image, View, Alert, TextInput } from 'react-native';
 import Colors from '../constants/Colors';
 import EventifyTextInput from '../components/EventifyTextInput';
 import Fontisto from '@expo/vector-icons/Fontisto';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Button from '../components/Button';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import AppConstants from '../constants/AppConstants';
 
 const EventifyLogo = require('../assets/eventify-logo.png');
@@ -14,9 +15,11 @@ export default function Register({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(false);
   const [loading, setLoading] = useState(false);
-
-
+  
   async function tryRegister () {
     if (name == "" || email == "" || password == "" || passwordCheck == "") {
         return Alert.alert('Completa los datos por favor');
@@ -36,7 +39,9 @@ export default function Register({ navigation }) {
             "name": name,
             "password": password,
             "email": email,
-            "birth_date": "2023-02-07",
+            "birth_date": (dateOfBirth.getFullYear()) + "-" +
+                         ((dateOfBirth.getMonth()+1).toString().padStart(2, '0')) + "-" + 
+                           dateOfBirth.getDate().toString().padStart(2, '0'),
             "money": 0,
             "profilePic": "string"
         })
@@ -68,7 +73,7 @@ export default function Register({ navigation }) {
         }
     }else{
         setLoading(false);
-        Alert.alert("Error al crear el usuario", "Ocurrio un error al intentar crear su perfil. Vuelva a intentarlo más tarde.");
+        Alert.alert("Error al crear el usuario2", "Ocurrio un error al intentar crear su perfil. Vuelva a intentarlo más tarde.");
     }
 
     navigation.navigate('Login');
@@ -88,6 +93,19 @@ export default function Register({ navigation }) {
                 <EventifyTextInput value={email} onChangeText={setEmail} name="Correo electronico" autoCapitalize={false} leftIcon={<Fontisto name="email" color={Colors.GRAY} size={23} />} inputTextColor={Colors.GRAY} fontSize={23} placeholderTextColor={Colors.GRAY} keyboardType="email-address" />
                 <EventifyTextInput value={password} onChangeText={setPassword} name="Clave" autoCapitalize={false} leftIcon={<Ionicons name="md-key-outline" color={Colors.GRAY} size={23} />} inputTextColor={Colors.GRAY} fontSize={23} placeholderTextColor={Colors.GRAY} isPassword={true} />     
                 <EventifyTextInput value={passwordCheck} onChangeText={setPasswordCheck} name="Repetir clave" autoCapitalize={false} leftIcon={<Ionicons name="md-key-outline" color={Colors.GRAY} size={23} />} inputTextColor={Colors.GRAY} fontSize={23} placeholderTextColor={Colors.GRAY} isPassword={true} />     
+                <EventifyTextInput value={selectedDate ? dateOfBirth.getDate().toString().padStart(2, '0')+"-"+((dateOfBirth.getMonth()+1).toString().padStart(2, '0'))+"-"+(dateOfBirth.getFullYear()): ''} onPressIn={()=>setShowDatePicker(true)} name="Fecha de Nacimiento" autoCapitalize={false} leftIcon={<Ionicons name="calendar" color={Colors.GRAY} size={23} />} inputTextColor={Colors.GRAY} fontSize={23} placeholderTextColor={Colors.GRAY} />
+                {
+                    showDatePicker ?
+                        <DateTimePicker value={dateOfBirth} onChange={(event) => {
+                            setDateOfBirth(new Date(event.nativeEvent.timestamp));
+                            setShowDatePicker(false);
+                            setSelectedDate(true);
+                        }}
+                        placeholder="Fecha y hora..." mode="date" caretHidden={true} maximumDate={new Date()}
+                    /> 
+                    : null
+                }            
+
                 <View style={{ width: '100%', height: '1000%' }}>
                     <Button onPress={tryRegister} buttonStyle={{ alignSelf: 'center', position: 'absolute', top: -10 }} title="INGRESAR" type="medium" titleSize={25} titleColor={Colors.WHITE} color={Colors.PRIMARY} />
                 </View>
@@ -97,3 +115,4 @@ export default function Register({ navigation }) {
   )
 }
 
+/*<DateTimePicker value={dateOfBirth} onDateChange={setDateOfBirth} placeholder="Fecha y hora..." mode="date" minimumDate={new Date()}/>*/
