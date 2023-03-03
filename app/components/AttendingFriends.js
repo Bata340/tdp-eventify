ff5a407bf454514d6f5d40b6109de23db9fec527
Maximiliano Petrucci <maximilianoangel.petrucci@gmail.com>
2023-03-02 21:16:08 -0300

    Fix al componente para que actualice ante un cambio

--- app/components/AttendingFriends.js
+++ app/components/AttendingFriends.js
@@ -10,27 +10,12 @@ import React from 'react';
 const MAX_SHOW_FRIENDS = 2;
 
 export default function AttendingFriends (props) {
-    const {usersFriends} = props;
-    const [friends, setFriends] = React.useState(usersFriends);
-
-    React.useEffect(() => {
-        setFriends(usersFriends)
-    }, [usersFriends])
-
-    const ShowFriends = () => {
-        let amount = friends.size < MAX_SHOW_FRIENDS? friends.size : MAX_SHOW_FRIENDS;
-        const friendsProfilePics = []
-        for (i; i < amount; i++){
-            friendsProfilePics.push(friends[i].profilePic);
-        }
-        return friendsProfilePics;
-    }
 
     return (
-        <View style={{ marginTop: 20, borderRadius: 20, backgroundColor: Colors.PRIMARY_DARK_GRAYED, paddingLeft: 25, paddingVertical: 25 }} onPress={()=>UsersList(friends)}>
-                <Text style={{ textAlign:"center", fontWeight: 'bold', color: Colors.WHITE, fontSize: 20, marginTop: 5 }}> <Entypo size={35} name="users" />Amigos que asisten {friends.length}
-                        <FlatList horizontal data={() => ShowFriends()} renderItem={({item}) => <UserAvatar uri={item.profilePic}/>}/>
+        <View style={{ marginTop: 20, borderRadius: 20, backgroundColor: Colors.PRIMARY_DARK_GRAYED, paddingLeft: 25, paddingVertical: 25 }} onPress={()=>UsersList(props.usersFriends)}>
+                <Text style={{ textAlign:"center", fontWeight: 'bold', color: Colors.WHITE, fontSize: 20, marginTop: 5 }}> <Entypo size={35} name="users" />Amigos que asisten {props.usersFriends.length}
                 </Text>
+                <FlatList horizontal data={props.usersFriends.map(f => f.profilePic).splice(0, 2)} renderItem={({item}) => <UserAvatar uri={item}/>}/>
         </View>
     )
 }
