3ecc343105756ed780e9877b73d6195f6bfbcfeb
agustina235 <asegura@fi.uba.ar>
2023-03-02 21:55:35 -0300

    Bug: attending friends
    Co-authored-by: Sol Fontenla <solfonte@users.noreply.github.com>
    Co-authored-by: Stephanieizquierdo <stephanieizquierdo@users.noreply.github.com>

--- app/components/AttendingFriends.js
+++ app/components/AttendingFriends.js
@@ -3,8 +3,12 @@ import UserAvatar from '../components/UserAvatar';
 import Entypo from '@expo/vector-icons/Entypo';
 import {View, Text, FlatList} from 'react-native';
 import Colors from '../constants/Colors';
-
 import React from 'react';
+import { ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { getFirebaseImage } from '../utils/FirebaseHandler';
+
+
 
 
 const MAX_SHOW_FRIENDS = 2;
@@ -12,29 +16,50 @@ const MAX_SHOW_FRIENDS = 2;
 export default function AttendingFriends (props) {
     const {usersFriends} = props;
     const [friends, setFriends] = React.useState(usersFriends);
+    const [profiles, setProfiles] = React.useState([])
 
     React.useEffect(() => {
         setFriends(usersFriends)
-    }, [usersFriends])
+        let pictures = ShowFriends().then((data)=> {return data})//promesa
+        setProfiles(pictures)
+    }, [usersFriends]) 
 
-    const ShowFriends = () => {
-        let amount = friends.size < MAX_SHOW_FRIENDS? friends.size : MAX_SHOW_FRIENDS;
+    const  ShowFriends = async () => {
         const friendsProfilePics = []
-        for (i; i < amount; i++){
-            friendsProfilePics.push(friends[i].profilePic);
+        
+        for (let i = 0; i < friends.length; i++){
+            let uri = await getImage(friends[i].profilePic).then((data) => { return data })
+            friendsProfilePics.push(uri);
         }
         return friendsProfilePics;
     }
 
+    async function getImage(persona){
+        let imageURI;
+        try{
+          imageURI = await getFirebaseImage('files/'+persona.profilePic);
+        }catch(exception){
+          //Image not available
+          imageURI = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png";
+        }
+        return imageURI
+      }
+   
+
     return (
-        <View style={{ marginTop: 20, borderRadius: 20, backgroundColor: Colors.PRIMARY_DARK_GRAYED, paddingLeft: 25, paddingVertical: 25 }} onPress={()=>UsersList(friends)}>
-                <Text style={{ textAlign:"center", fontWeight: 'bold', color: Colors.WHITE, fontSize: 20, marginTop: 5 }}> <Entypo size={35} name="users" />Amigos que asisten {friends.length}
-                        <FlatList horizontal data={() => ShowFriends()} renderItem={({item}) => <UserAvatar uri={item.profilePic}/>}/>
+        <View style={{ marginTop: 20, borderRadius: 20, backgroundColor: Colors.PRIMARY_DARK_GRAYED, paddingLeft: 25, paddingVertical: 25, }}  onPress={()=>UsersList(friends)}>
+                <Text style={{ textAlign:"center", fontWeight: 'bold', color: Colors.WHITE, fontSize: 20, marginTop: 5 }}> <Entypo size={35} name="users" />Amigos que asisten {usersFriends.lenght}
                 </Text>
+                <FlatList horizontal data={props.usersFriends.map(async () => profiles.splice(0, 3))} renderItem={({item}) => <UserAvatar uri={item}/>}/>
+
         </View>
     )
 }
+//</ScrollView>
 
+//<FlatList horizontal data={DATA} renderItem={renderItem} /> 
+//
 
-
-
+//<View style={{flexDirection:"row", alignItems:"center", marginHorizontal:50}}>
+//<UserAvatar size={20} uri={appAuthContext.userSession.getUserAvatar()} />
+//</View>
--- app/components/UserAvatar.js
+++ app/components/UserAvatar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Image, View } from 'react-native';
 
+
 export default function UserAvatar({
     uri = "",
     size = 30
 }) {
+
+    const imagePath = uri !== ""? { uri: uri }: {uri : "https://as2.ftcdn.net/v2/jpg/02/15/84/43/1000_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"}
+
     return(
         <View style={{ borderRadius: size * 2, width: size, height: size, overflow: 'hidden', alignItems: 'center' }}>
             <Image
                 style={{ height: size, width: size }}
-                source={{ uri: uri }}
+                
+                source={imagePath}
             />
         </View>
     )
